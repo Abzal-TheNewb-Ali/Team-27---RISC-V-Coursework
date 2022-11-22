9a3d2976e56c2db26b9d81e99f68a8414296fb1d
ybelur <105719713+ybelur@users.noreply.github.com>
2022-11-22 17:11:11 +0000

    Update PC_Unit.sv

--- PC_Unit/PC_Unit.sv
+++ PC_Unit/PC_Unit.sv
@@ -4,11 +4,13 @@ module PC_Unit(
     input  logic        PCsrc,
     input  logic [31:0] ImmOp,
     output logic [31:0] pc);
-
+    
+    //Interconnect Wires
     logic   [31:0]  next_pc;
     logic   [31:0]  branch_pc;
     logic   [31:0]  inc_pc;
 
+//PC Register 32-bit flip flop submodule
 flop PC_Reg (
     .clk (clk),
     .rst (rst),
@@ -16,17 +18,20 @@ flop PC_Reg (
     .q   (pc)
 );
 
+//+4 increment submodule 
 adder4 PC_Inc (
     .add_in     (pc),
     .inc_result (inc_pc)
 );
 
+//PC + ImmOp adder 32-bit
 adder_32bit branch_adder(
     .add1    (pc),
     .add2    (ImmOp),
     .result  (branch_pc)
 );
 
+    //PC Type multiplexer
     assign next_pc = PCsrc ? branch_pc : inc_pc;
 
 endmodule
